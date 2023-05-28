eadb2c24a3fff4daac3ce1387e702bc49b4d98ae
craole-cc <craole-cc@proton.me>
2023-05-28 16:30:34 -0500

    Getting ready to add a desktop

--- flake.nix
+++ flake.nix
@@ -1,6 +1,9 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs = {
+      # url = "github:nixos/nixpkgs/nixos-unstable";
+      url = "github:nixos/nixpkgs/nixos-22.11";
+    };
     home-manager = {
       url = "github:nix-community/home-manager/release-22.11";
       inputs.nixpkgs.follows = "nixpkgs";
