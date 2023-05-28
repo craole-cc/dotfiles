40091aa3bbbec79a5447ead76dca19d9d4ead4b6
craole-cc <craole-cc@proton.me>
2023-05-28 17:41:12 +0200

    trying with Gnome

--- flake.nix
+++ flake.nix
@@ -1,12 +1,11 @@
 {
   ## ensure a successful installation by pinning nixpkgs to a known
   ## good revision
-  inputs.nixpkgs.url =
-    "github:nixos/nixpkgs/c8f6370f7daf435d51d137dcbd80c7ebad1f21f2";
+  #inputs.nixpkgs.url = "github:nixos/nixpkgs/c8f6370f7daf435d51d137dcbd80c7ebad1f21f2";
   ## after reboot, you can track latest stable by using
   #inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
   ## or track rolling release by using
-  #inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }@inputs:
     let
