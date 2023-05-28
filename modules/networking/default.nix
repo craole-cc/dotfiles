5e38844875b0a7855c918694865303500a55e65f
craole-cc <craole-cc@proton.me>
2023-05-28 19:39:52 +0200

    Added modules from ne9z-personal to initialize per-user

--- modules/networking/default.nix
+++ modules/networking/default.nix
@@ -4,21 +4,9 @@
   ...
 }: let
   cfg = config.zfs-root.networking;
-  inherit (lib) types mkDefault mkOption mkIf mkMerge mapAttrsToList;
+  inherit (lib) types mkDefault mkOption;
 in {
   options.zfs-root.networking = {
-    networkmanager = {
-      enable = mkOption {
-        type = types.bool;
-        default = true;
-      };
-      wirelessNetworks = mkOption {
-        description = "configure wireless networks with NetworkManager";
-        type = types.attrsOf types.str;
-        default = {};
-        example = {"network_ssid" = "password";};
-      };
-    };
     hostName = mkOption {
       description = "The name of the machine.  Used by nix flake.";
       type = types.str;
@@ -39,32 +27,6 @@ in {
     networking = {
       firewall.enable = mkDefault true;
       hostName = cfg.hostName;
-      networkmanager = {
-        enable = cfg.networkmanager.enable;
-        wifi = {macAddress = mkDefault "random";};
-      };
     };
-    environment.etc = mkIf cfg.networkmanager.enable (mkMerge (mapAttrsToList
-      (name: pwd: {
-        "NetworkManager/system-connections/${name}.nmconnection" = {
-          # networkmanager demands secure permission
-          mode = "0600";
-          text = ''
-            [connection]
-            id=${name}
-            type=wifi
-
-            [wifi]
-            mode=infrastructure
-            ssid=${name}
-
-            [wifi-security]
-            auth-alg=open
-            key-mgmt=wpa-psk
-            psk=${pwd}
-          '';
-        };
-      })
-      cfg.networkmanager.wirelessNetworks));
   };
 }
