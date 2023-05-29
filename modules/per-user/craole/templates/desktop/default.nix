9cfd3bd014293d168b5ac9d991028159a854ecaa
craole-cc <craole-cc@proton.me>
2023-05-28 23:32:54 -0500

    Getting ready to add a desktop

--- modules/per-user/craole/templates/desktop/default.nix
+++ modules/per-user/craole/templates/desktop/default.nix
@@ -89,10 +89,8 @@ in {
     services = {
       xserver = {
         enable = true;
-        desktopManager = {
-          gnome.enable = true;
-          gdm.enable = true;
-        };
+        desktopManager.gnome.enable = true;
+         displayManager.gdm.enable = true;
       };
       blueman.enable = true;
       logind = {
