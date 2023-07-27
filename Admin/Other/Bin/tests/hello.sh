6145dbfc21345f99121bb393d766002c3bb710e1
craole <iamcraole@gmail.com>
2023-07-26 21:55:00 -0500

    "NixWSL bin"

--- Admin/Other/Bin/tests/hello.sh
+++ Admin/Other/Bin/tests/hello.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
-
-msg="Hello World!"
-timestamp="$(date)"
-
-printf "%s\nIt's %s" "$msg" "$timestamp"
+#!/bin/sh
+
+msg="Hello World!"
+
+timestamp="$(date)"
+
+printf "%s\nIt's %s" "$msg" "$timestamp"
