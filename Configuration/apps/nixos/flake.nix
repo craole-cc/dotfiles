f52f4c2a39c36b98bdd0a875313ac2ddc1263c1b
Craole <32288735+Craole@users.noreply.github.com>
2024-11-04 00:16:25 -0500

    I forgot which config I use for preci

--- Configuration/apps/nixos/flake.nix
+++ Configuration/apps/nixos/flake.nix
@@ -57,7 +57,7 @@
         dbook = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [
-            "./imports/dbook"
+            ./imports/dbook
           ] ++ coreModules ++ homeModules;
         };
       };
