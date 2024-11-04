7d3bbc31be35db2187699608f1bd9084ead2faff
Craole <32288735+Craole@users.noreply.github.com>
2024-11-04 10:53:44 -0500

    moved flake to the git root

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "NixOS Configuration Flake";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    # nixed = {
+    #   url = "github:Craole/nixed";
+    #   flake = false;
+    # };
+  };
+  outputs =
+    inputs@{ self, ... }:
+    let
+      inherit (inputs.nixpkgs.lib) nixosSystem;
+      inherit (inputs.home-manager.nixosModules) home-manager;
+      src = ./Configuration/apps/nixos;
+      mods = {
+        core =
+          [
+            (src + "/core/libraries")
+            (src + "/core/options")
+          ]
+          ++ [
+            home-manager
+            {
+              home-manager = {
+                backupFileExtension = "BaC";
+                useGlobalPkgs = true;
+                useUserPackages = true;
+              };
+            }
+          ];
+        home = [
+          (src + "/home/libraries")
+          (src + "/home/options")
+        ];
+      };
+    in
+    {
+      nixosConfigurations = {
+        preci = nixosSystem {
+          system = "x86_64-linux";
+          modules = mods.core ++ [ ./core/configurations/preci ];
+        };
+        dbook = nixosSystem {
+          system = "x86_64-linux";
+          modules = mods.core ++ [ ./core/configurations/dbook ];
+        };
+      };
+    };
+}
