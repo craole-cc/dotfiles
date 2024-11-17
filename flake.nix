8e1df11c550cca66a2f02b91996c0e6e8a1b2f38
craole-cc <craole-cc@gmail.com>
2024-11-17 13:06:02 -0500

    Flake Update

--- flake.nix
+++ flake.nix
@@ -143,7 +143,7 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-unstable";
+      url = "github:nixos/nixpkgs/nixos-24.05";
     };
     nixpkgs-unstable = {
       url = "github:nixos/nixpkgs/nixos-unstable";
