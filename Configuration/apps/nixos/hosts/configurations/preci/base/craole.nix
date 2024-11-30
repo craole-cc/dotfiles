5d79b31481f3f235c6bbd0c6acdfd4c0b65e60b2
craole-cc <craole-cc@gmail.com>
2024-11-30 13:59:23 -0500

    Added plasmaManager to flake

--- Configuration/apps/nixos/hosts/configurations/preci/base/craole.nix
+++ Configuration/apps/nixos/hosts/configurations/preci/base/craole.nix
@@ -88,22 +88,25 @@ in
           victor-mono
           zed-mono
         ])
-        ++ stdenv.mkDerivation {
-          pname = "Awesome-Fonts";
-          version = "1.0";
+        ++ [
+          stdenv.mkDerivation
+          {
+            pname = "Awesome-Fonts";
+            version = "1.0";
 
-          src = pkgs.fetchFromGitHub {
-            owner = "rng70";
-            repo = "Awesome-Fonts";
-            rev = "3733f56e431608878d6cbbf2d70d8bf36ab2c226";
-            sha256 = "0m41gdgp06l5ymwvy0jkz6qfilcz3czx416ywkq76z844y5xahd0";
-          };
+            src = pkgs.fetchFromGitHub {
+              owner = "rng70";
+              repo = "Awesome-Fonts";
+              rev = "3733f56e431608878d6cbbf2d70d8bf36ab2c226";
+              sha256 = "0m41gdgp06l5ymwvy0jkz6qfilcz3czx416ywkq76z844y5xahd0";
+            };
 
-          installPhase = ''
-            mkdir -p $out/share/fonts/opentype
-            find $src -type f \( -name '*.ttf' -o -name '*.otf' \) -exec cp {} $out/share/fonts/opentype/ \;
-          '';
-        };
+            installPhase = ''
+              mkdir -p $out/share/fonts/opentype
+              find $src -type f \( -name '*.ttf' -o -name '*.otf' \) -exec cp {} $out/share/fonts/opentype/ \;
+            '';
+          }
+        ];
 
       sessionVariables = {
         EDITOR = "hx";
