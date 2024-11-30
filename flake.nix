be50f4aaf6c9861de0cd570c2cf955b05a9de122
craole-cc <craole-cc@gmail.com>
2024-11-30 04:14:41 -0500

    Worked on geet

--- flake.nix
+++ flake.nix
@@ -107,14 +107,14 @@
                 NIXOS_FLAKE = flake.local;
               };
               shellAliases = {
-                Flake = ''pushd ${paths.flake.local} && { { command -v geet && geet ;} || git add --all; git commit --message "Flake Update" ;} && sudo nixos-rebuild switch --flake . --show-trace; popd'';
+                Flake = ''pushd ${paths.flake.local} && { { { command -v geet && geet ;} || git add --all; git commit --message "Flake Update" ;} ; sudo nixos-rebuild switch --flake . --show-trace ;}; popd'';
                 Flush = ''sudo nix-collect-garbage --delete-old; sudo nix-store --gc'';
-                Flash=''Flake'';
+                Flash = ''geet --path ${paths.flake.local} && sudo nixos-rebuild switch --flake ${paths.flake.local} --show-trace'';
                 Flick = ''Flush && Flash && sudo reboot'';
-                Reboot=''leave --reboot'';
-                Reload=''leave --logout'';
-                Retire=''leave --shutdown'';
-                Q=''leave --terminal''
+                Reboot = ''leave --reboot'';
+                Reload = ''leave --logout'';
+                Retire = ''leave --shutdown'';
+                Q = ''leave --terminal'';
                 ".." = "cd .. || return 1";
                 "..." = "cd ../.. || return 1";
                 "...." = "cd ../../.. || return 1";
