ccd46216c2839582769f865696cc7a39ec600dc9
craole-cc <craole-cc@gmail.com>
2024-11-30 14:01:03 -0500

    Added plasmaManager to flake

--- Configuration/apps/nixos/hosts/configurations/preci/base/craole.nix
+++ Configuration/apps/nixos/hosts/configurations/preci/base/craole.nix
@@ -66,13 +66,12 @@ in
           via
           vscode-fhs
           qbittorrent
-          # mpv
           inkscape-with-extensions
           darktable
           ansel
 
           #| Fonts
-          # fontsMono
+          fontsMono
           lexend
           material-design-icons
           material-icons
@@ -87,23 +86,7 @@ in
           monoid
           victor-mono
           zed-mono
-        ])
-        ++ (stdenv.mkDerivation {
-          pname = "Awesome-Fonts";
-          version = "1.0";
-
-          src = pkgs.fetchFromGitHub {
-            owner = "rng70";
-            repo = "Awesome-Fonts";
-            rev = "3733f56e431608878d6cbbf2d70d8bf36ab2c226";
-            sha256 = "0m41gdgp06l5ymwvy0jkz6qfilcz3czx416ywkq76z844y5xahd0";
-          };
-
-          installPhase = ''
-            mkdir -p $out/share/fonts/opentype
-            find $src -type f \( -name '*.ttf' -o -name '*.otf' \) -exec cp {} $out/share/fonts/opentype/ \;
-          '';
-        });
+        ]);
 
       sessionVariables = {
         EDITOR = "hx";
