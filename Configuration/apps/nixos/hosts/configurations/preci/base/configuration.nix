bb1b87cfdf03e23fb815afc6268139486c33422b
craole-cc <craole-cc@gmail.com>
2024-11-30 21:11:44 -0500

    disabling relogin

--- Configuration/apps/nixos/hosts/configurations/preci/base/configuration.nix
+++ Configuration/apps/nixos/hosts/configurations/preci/base/configuration.nix
@@ -84,7 +84,7 @@ in
       sddm = {
         enable = true;
         wayland.enable = true;
-        autoLogin.relogin = true;
+        # autoLogin.relogin = true; #TODO This doesn't work
       };
     };
 
