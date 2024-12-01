072fa107a98b713c895308b3d02e94881f0c1a32
craole-cc <craole-cc@gmail.com>
2024-11-30 19:01:53 -0500

    include plasma.nix

--- Configuration/apps/nixos/hosts/configurations/preci/base/craole/plasma.nix
+++ Configuration/apps/nixos/hosts/configurations/preci/base/craole/plasma.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   programs.plasma = {
-    enable = true;
+    enable = false;
     # workspace = {
     #   clickItemTo = "open"; # If you liked the click-to-open default from plasma 5
     #   lookAndFeel = "org.kde.breezedark.desktop";
