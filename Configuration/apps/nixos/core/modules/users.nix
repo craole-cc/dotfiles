aed48ca51009679728809c883b724247eb43454c
craole-cc <craole-cc@gmail.com>
2024-12-23 01:40:04 -0500

    flattening config

--- Configuration/apps/nixos/core/modules/users.nix
+++ Configuration/apps/nixos/core/modules/users.nix
@@ -2,6 +2,7 @@
 let
   inherit (specialArgs) users;
   inherit (lib.attrsets) mapAttrs;
+  inherit (config.networking) networkmanager;
 in
 {
   users.users = mapAttrs (name: user: {
@@ -9,6 +10,14 @@ in
     description = user.description or name;
     isNormalUser = user.isNormalUser or true;
     hashedPassword = user.hashedPassword or null;
+    extraGroups = (
+      if user.isNormalUser then
+        [ "users" ]
+        ++ (if networkmanager.enable then [ "networkmanager" ] else [ ])
+        ++ (if user.isAdminUser then [ "wheel" ] else [ ])
+      else
+        [ ]
+    );
   }) specialArgs.users;
   # users = {
   #   users = mapAttrs (
