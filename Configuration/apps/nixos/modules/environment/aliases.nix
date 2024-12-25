040fff14f5debdd769c74e0cb8c2c31f03118f16
craole-cc <craole-cc@gmail.com>
2024-12-25 11:20:30 -0500

    trying the symlink approach

--- Configuration/apps/nixos/modules/environment/aliases.nix
+++ Configuration/apps/nixos/modules/environment/aliases.nix
@@ -5,24 +5,28 @@ let
 in
 {
   environment.shellAliases = {
-    Flake = ''pushd ${flake.local} && { { { command -v geet && geet ;} || git add --all; git commit --message "Flake Update" ;} ; sudo nixos-rebuild switch --flake . --show-trace ;}; popd'';
-    Flush = ''sudo nix-collect-garbage --delete-old; sudo nix-store --gc'';
-    Flash = ''geet --path ${flake.local} && sudo nixos-rebuild switch --flake ${flake.local} --show-trace'';
+    Flake = ''{ { { command -v geet && geet ;} || git add --all; git commit --message "Flake Update" ;} ; sudo nixos-rebuild switch --flake . --show-trace ;}'';
+    Flash-local = ''geet --path ${flake.local} && sudo nixos-rebuild switch --flake ${flake.local} --show-trace'';
     Flash-root = ''geet --path ${flake.root} && sudo nixos-rebuild switch --flake ${flake.root} --show-trace'';
     Flash-link = ''geet --path ${flake.link} && sudo nixos-rebuild switch --flake ${flake.link} --show-trace'';
-    "..local" = ''cd ${flake.local} || exit'';
-    "..root" = ''cd ${flake.root} || exit'';
-    "..link" = ''cd ${flake.link} || exit'';
+    Flash = ''Flash-local'';
     Flick = ''Flush && Flash && Reboot'';
+    Flick-local = ''Flush && Flash-local && Reboot'';
+    Flick-root = ''Flush && Flash-root && Reboot'';
+    Flick-link = ''Flush && Flash-link && Reboot'';
+    Flush = ''sudo nix-collect-garbage --delete-old; sudo nix-store --gc'';
     Reboot = ''leave --reboot'';
     Reload = ''leave --logout'';
     Retire = ''leave --shutdown'';
     Q = ''kill -KILL "$(ps -o ppid= -p $$)"'';
     q = ''leave --terminal'';
-    ".." = "cd .. || return 1";
-    "..." = "cd ../.. || return 1";
-    "...." = "cd ../../.. || return 1";
-    "....." = "cd ../../../.. || return 1";
     h = "history";
+    ".dots-local" = ''cd ${flake.local}'';
+    ".dots-root" = ''cd ${flake.root}'';
+    ".dots-link" = ''cd ${flake.link}'';
+    ".." = "cd ..";
+    "..." = "cd ../..";
+    "...." = "cd ../../..";
+    "....." = "cd ../../../..";
   };
 }
