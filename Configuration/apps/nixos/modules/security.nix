39bb97363f9efcec259cd9a5f0a2cbf967fc5542
craole-cc <craole-cc@gmail.com>
2024-12-25 00:07:08 -0500

    flattened the config

--- Configuration/apps/nixos/modules/security.nix
+++ Configuration/apps/nixos/modules/security.nix
@@ -1,17 +1,17 @@
 { specialArgs, lib, ... }:
 let
-  inherit (specialArgs.host) users host;
+  inherit (specialArgs.host) people userConfigs;
   inherit (lib.attrsets) attrNames filterAttrs;
   inherit (lib.lists) unique filter elem;
 
-  enabledUsers = filter (user: user.enable or true) host.people;
+  enabledUsers = filter (user: user.enable or true) people;
   regularUsersPerHost = map (user: user.name) (
     filter (user: user.admin or false == false) enabledUsers
   );
   elevatedUsersPerHostConf = map (user: user.name) (
     filter (user: user.admin or false == true) enabledUsers
   );
-  elevatedUsersPerUserConf = attrNames (filterAttrs (_: user: user.isAdminUser or false) users);
+  elevatedUsersPerUserConf = attrNames (filterAttrs (_: user: user.isAdminUser or false) userConfigs);
   elevatedUsers = unique (
     filter (user: !elem user regularUsersPerHost) (elevatedUsersPerHostConf ++ elevatedUsersPerUserConf)
   );
