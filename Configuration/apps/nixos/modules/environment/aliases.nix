a6ab80a6a37220b4ab905d6f5da8037410851d44
craole-cc <craole-cc@gmail.com>
2024-12-26 22:40:52 -0500

    moved to '~/.dots'

--- Configuration/apps/nixos/modules/environment/aliases.nix
+++ Configuration/apps/nixos/modules/environment/aliases.nix
@@ -7,12 +7,12 @@ in
     Flake = ''if command -v geet ; then geet ; else git add --all; git commit --message "Flake Update" ; fi ; sudo nixos-rebuild switch --flake . --show-trace'';
     Flash-local = ''geet --path ${flake.local} && sudo nixos-rebuild switch --flake ${flake.local} --show-trace'';
     Flash-root = ''geet --path ${flake.root} && sudo nixos-rebuild switch --flake ${flake.root} --show-trace'';
-    Flash-link = ''geet --path ${flake.link} && sudo nixos-rebuild switch --flake ${flake.link} --show-trace'';
+    # Flash-link = ''geet --path ${flake.link} && sudo nixos-rebuild switch --flake ${flake.link} --show-trace'';
     Flash = ''Flash-local'';
     Flick = ''Flush && Flash && Reboot'';
     Flick-local = ''Flush && Flash-local && Reboot'';
     Flick-root = ''Flush && Flash-root && Reboot'';
-    Flick-link = ''Flush && Flash-link && Reboot'';
+    # Flick-link = ''Flush && Flash-link && Reboot'';
     Flush = ''sudo nix-collect-garbage --delete-old; sudo nix-store --gc'';
     Reboot = ''leave --reboot'';
     Reload = ''leave --logout'';
